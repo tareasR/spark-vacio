e27c23bfcb7be3d5dc20148e28d72b35834f6edc
tareasR <tareasrojano@gmail.com>
2024-04-18 22:48:36 -0600

    testing

---
+++ src/main/java/mx/uv/App.java
@@ -0,0 +1,34 @@
+package mx.uv;
+
+import static spark.Spark.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        port (8080);
+
+        //fuente:https://gist.github.com/saeidzebardast/e375b7d17be3e0f4dddf
+        options("/*",(request,response)->{
+        String accessControlRequestHeaders=request.headers("Access-Control-Request-Headers");
+        if(accessControlRequestHeaders!=null){
+        response.header("Access-Control-Allow-Headers",accessControlRequestHeaders);
+        }
+        String accessControlRequestMethod=request.headers("Access-Control-Request-Method");
+        if(accessControlRequestMethod!=null){
+        response.header("Access-Control-Allow-Methods",accessControlRequestMethod);
+        }
+        return "OK";
+        });
+        before((request,response)->response.header("Access-Control-Allow-Origin","*"));
+
+        get("/", (req, res)->{
+            return null;
+        });
+    }
+}
